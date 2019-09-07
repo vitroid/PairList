fd4c5c603c0053f77e494ffbad25136cfb6a6975
Masakazu Matsumoto <vitroid@gmail.com>
2019-09-07 13:49:20 +0900

    .

--- replacer.py
+++ replacer.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python
 import sys
 import os
-from genice.tool import line_replacer
 import distutils.core
+from logging import getLogger
 
 setup = distutils.core.run_setup("setup.py")
+
+
+
+def line_replacer(line, d):
+    logger = getLogger()
+    s = ""
+    for tag in d:
+        loc = line.find(tag)
+        if loc >= 0:
+            logger.debug("From {0} by {1}.".format(tag, d[tag]))
+            replacement = d[tag].splitlines()
+            if len(replacement) == 1:
+                s = line.replace(tag, replacement[0])
+            else:
+                indent = line[:loc]
+                for newline in replacement:
+                    s += indent + newline + "\n"
+            return s
+    return line
+
+
+
 
 d = {
     "%%sample.py%%"   : "".join(open("sample.py").readlines()),
